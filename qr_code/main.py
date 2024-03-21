b743e99ae826e9e5a85c2eebc7e8d34c6c506801
jd05 <124862332+jdd050@users.noreply.github.com>
2024-03-20 19:41:25 -0500

    ui progress

--- qr_code/main.py
+++ qr_code/main.py
@@ -1,9 +1,10 @@
 import os
 from PIL import ImageTk, Image
 import qrcode
-import tkinter as tk
+import tkinter
+from tkinter import filedialog
 
-class Main(tk.Tk):
+class Main(tkinter.Tk):
     # create the root window
     def __init__(self):
         super().__init__()
@@ -16,52 +17,69 @@ class Main(tk.Tk):
     # create a layout for the window content
     def organize_content(self) -> None:
         # define the top area
-        self.top_rectangle = tk.Frame(self.master, background="red", width=800, height=75)
+        self.top_rectangle = tkinter.Frame(self.master, background="red", width=800, height=75)
         self.top_rectangle.grid(column=0, row=0, columnspan=2, rowspan=1, padx=0, pady=0)
         # define middle left
-        self.left = tk.Frame(self.master, background="Gray", width=400, height=450)
+        self.left = tkinter.Frame(self.master, background="Gray", width=400, height=525)
         self.left.grid(column=0, row=1, columnspan=1, rowspan=1, padx=0, pady=0)
         # define middle right
-        self.right = tk.Frame(self.master, background="black", width=400, height=450)
+        self.right = tkinter.Frame(self.master, background="black", width=400, height=525)
         self.right.grid(column=1, row=1, columnspan=1, rowspan=1, padx=0, pady=0)
-        # define bottom
-        self.bottom_rectangle = tk.Frame(self.master, background="blue", width=800, height=75)
-        self.bottom_rectangle.grid(column=0, row=2, columnspan=2, rowspan=1, padx=0, pady=0)
         return None
     
+    # function for opening images in the image entry
+    def open_image(self) -> None:
+        image_filename = filedialog.askopenfilename()
+        print(image_filename)
+        self.image_file = Image.open(image_filename)
+        return None
+        
     # function responsible for creating and managing the user interface
     def ui(self) -> None:
         ### TOP ###
         # create the title for the app
-        title = tk.Label(self.top_rectangle, text="Python QR Code Generator", font=("Helvetica bold", 26, "bold"))
-        title.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-        ### MID-LEFT ###
-        left_header = tk.Label(self.left, text="Options", font=("Helvetica bold", 13))
-        left_header.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
-        '''
-        doesnt work because .grid() doesnt want to exist (idk tbh)
+        title = tkinter.Label(self.top_rectangle, text="Python QR Code Generator", font=("Helvetica bold", 26, "bold"))
+        title.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
         
-        # filler for formatting
-        filler_one = tk.Label(self.left)
-        filler_one.grid(row=0, column=0)
-        # section title
-        left_header = tk.Label(self.left, text="Options", font=("Helvetica bold", 13))
-        left_header.grid(row=0, column=1)
-        # filler for formatting
-        filler_two = tk.Label(self.left)
-        filler_two.grid(row=0, column=2)
-        '''
+        ### MID-LEFT ###
+        left_header = tkinter.Label(self.left, text="Options", font=("Helvetica bold", 13))
+        left_header.place(relx=0.5, rely=0.1, anchor=tkinter.CENTER)
+        # text data entry
+        self.text_var = tkinter.StringVar()
+        text_data = tkinter.Entry(self.left, textvariable=self.text_var, width=50)
+        text_data.place(relx=0.55, rely=0.2, anchor=tkinter.CENTER)
+        text_label = tkinter.Label(self.left, text="Text:", font=("Helvetica bold", 13, "bold"), bg="black", fg="white")
+        text_label.place(relx=0.1, rely=0.2, anchor=tkinter.CENTER)
+        # image entry
+        image_button = tkinter.Button(self.left, text="Choose an Image", command=self.open_image)
+        image_button.place(relx=0.35, rely=0.3, anchor=tkinter.CENTER)
+        image_label = tkinter.Label(self.left, text="Image:", font=("Helvetica bold", 13, "bold"), bg="black", fg="white")
+        image_label.place(relx=0.1, rely=0.3, anchor=tkinter.CENTER)
+        # link entry
+        self.link_var = tkinter.StringVar()
+        link_data = tkinter.Entry(self.left, textvariable=self.link_var, width=50)
+        link_data.place(relx=0.55, rely=0.4, anchor=tkinter.CENTER)
+        link_label = tkinter.Label(self.left, text="Link:", font=("Helvetica bold", 13, "bold"), bg="black", fg="white")
+        link_label.place(relx=0.1, rely=0.4, anchor=tkinter.CENTER)
+        # generate button
+        generate_button = tkinter.Button(self.left, text="Generate QR Code", command=lambda:print("generate button pressed"))
+        generate_button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
+        
         ### MID-RIGHT ###
         # the qr code
         self.generate_qr_code()
+        # save button
+        save_button = tkinter.Button(self.right, text="Save QR Code", command=lambda:print("save button pressed"))
+        save_button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
         return None
     
     # placeholder for now
     def generate_qr_code(self) -> None:
         self.qr_code_img = Image.open(r"qr_code\granny.png")
         self.qr_code_photo = ImageTk.PhotoImage(self.qr_code_img.resize((250, 300)))
-        label = tk.Label(self.right, image=self.qr_code_photo, width=250, height=300)
-        label.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+        label = tkinter.Label(self.right, image=self.qr_code_photo, width=250, height=300)
+        label.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
+        
 
 instance = Main()
 
